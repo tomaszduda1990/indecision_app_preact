87e4dc841193b99c077849791abead59ec1a991d
Tomasz Duda <tomaszduda1990@users.noreply.github.com>
2018-09-10 16:23:49 +0200

    styles added

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import IndecisionApp from "./components/IndecisionApp";
 import "normalize.css";
 import "./styles/styles.scss";
 
+const meta = document.createElement("meta");
+meta.name = "viewport";
+meta.content = "width=device-width, initial-scale=1";
+document.head.appendChild(meta);
+
 render(<IndecisionApp />, document.getElementById("root"));
